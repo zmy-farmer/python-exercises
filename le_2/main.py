058131b3d0a4d7f51f16e7f5cb252b745330e1e9
ameng <734738515@qq.com>
2016-06-10 12:01:29 +0800

    生成字母和随机数的优惠劵或激活码完成

---
+++ le_2/main.py
@@ -0,0 +1,18 @@
+# _*_ coding:utf-8 _*_
+import random, string  
+  
+def rand_str(num, length = 7):  
+    f = open('./le_2/yhm.txt','wb')
+    for i in range(num):
+        #所有字母和数字
+        chars = string.letters +string.digits #abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
+        #random.choice从序列中获取一个随机元素
+        #random.sample的函数原型为：random.sample(sequence, k)，从指定序列中随机获取指定长度的片断。sample函数不会修改原有序列。
+        # s = [random.choice(chars) for j in range(length)]
+        s = string.join(random.sample(chars, 7)).replace(" ","")
+        f.write(s+"\r\n")
+    f.close()  
+    print "DONE!"  
+if __name__ == '__main__':  
+    rand_str(10)
+
